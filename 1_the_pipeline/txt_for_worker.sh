2f3b34a8dee107191e1ec45520bb500bbba16449
Yan Zhao <zhaoyanz@stu.xjtu.edu.cn>
2022-07-04 16:15:06 +0200

    revise text_for_worker.sh
    add also command for getting the content of label.txt file.

--- 1_the_pipeline/txt_for_worker.sh
+++ 1_the_pipeline/txt_for_worker.sh
@@ -1,7 +1,14 @@
-raw_dir="/staging/leuven/stg_00019/margot_research/220322_Microhaps_CC115_Mar22"
+raw_dir="/staging/leuven/stg_00019/margot_research/220614_Microhaps_CC112_June22"
 
 echo SAMPLE>microhap.txt
 ls $raw_dir|grep ".R"|awk -F ".R" '{print $1}'|uniq>>microhap.txt
 
 echo SAMPLE>merge.txt
 cat microhap.txt|grep -v SAMPLE|awk -F . '{print $1}'|uniq>>merge.txt
+
+
+# get label.txt for microhaplot
+
+cat change_name.txt|grep -v SAMPLE|awk -F ',' '{print $2".sam\t.\t"$2}'
+
+# remove extra spaces 
