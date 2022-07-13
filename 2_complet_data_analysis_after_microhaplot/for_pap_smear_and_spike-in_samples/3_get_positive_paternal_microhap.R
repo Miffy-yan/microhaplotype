e0df77e43b23827b43740f8149d9e92b1179cc48
Yan Zhao <zhaoyanz@stu.xjtu.edu.cn>
2022-07-13 20:38:34 +0200

    change fold range to 0.4-2.2
    1. change range to mean+-3sd;
    2. add code to get  all pat_match automatically.

--- 2_complet_data_analysis_after_microhaplot/for_pap_smear_and_spike-in_samples/3_get_positive_paternal_microhap.R
+++ 2_complet_data_analysis_after_microhaplot/for_pap_smear_and_spike-in_samples/3_get_positive_paternal_microhap.R
@@ -1,8 +1,8 @@
-min_reads <- 50 
+min_reads <- 40 
 lower_lim<-0.4
-upper_lim<-2.0
-# fold range 3sd: 2.0-0.4 95.8% control sample fall within this region
-wd<-"/Users/miffy/OneDrive - KU Leuven/0_working directory/microhap_PAP-smears/19_new_pap_samples/CC112/microhap_raw_resuts/CC112_batch3"
+upper_lim<-2.2
+# fold range mean+-4sd
+wd<-"/Users/miffy/OneDrive - KU Leuven/0_working directory/microhap_PAP-smears/19_new_pap_samples/1st_finished_samples/CC125/microhap_raw_results/CC125_batch3"
 setwd(wd)
 files<-list.files(path = wd,pattern = "*_fold.txt")
 
@@ -14,11 +14,11 @@ for(file in files){
   chrs<-unique(data$chr)
   data_select<-matrix(0,0,8)
   for (chr in chrs) {
-    data_chr<-data[data$chr==chr&data$num>50,]
+    data_chr<-data[data$chr==chr&data$num>min_reads,]
     data_chr_select<-data_chr[data_chr$fold<lower_lim|data_chr$fold>upper_lim,]
     data_select<-rbind(data_select,data_chr_select)
   }
-  write.table(data_select,file = paste(name,"selected_according_to_fold_range.txt",sep="_"),quote = F)
+  write.table(data_select,file = paste(name,"selected_according_to_fold_range",lower_lim,upper_lim,"min_reads",min_reads,".txt",sep="_"),quote = F)
 }
 
 
--- 2_complet_data_analysis_after_microhaplot/for_pap_smear_and_spike-in_samples/4_extract_final_results_from_all_batches.sh
+++ 2_complet_data_analysis_after_microhaplot/for_pap_smear_and_spike-in_samples/4_extract_final_results_from_all_batches.sh
@@ -2,9 +2,19 @@ cd *** CC11*
 
 for i in `ls -d *_batch?`
 do 
-for sample in `ls $i/*range*`
+for sample in `ls $i/*range_0.4_2.2_min_reads_40_.txt`
 do
 echo $sample"\t"$i 
 cat $sample|awk '{print}'
 done
-done>CC120_results.txt
+done>CC125_results.txt
+
+
+for i in `ls -d *_batch?`
+do 
+for sample in `ls $i/*pat_matches.txt`
+do
+echo $sample"\t"$i 
+cat $sample|awk '{print}'
+done
+done>CC125_pat_match.txt
